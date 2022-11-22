63c5b103f69e0332b234ac1d28a78c356b9151e0
Junrong Fu <junrongfu6@gmail.com>
2022-11-22 22:57:06 +0800

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">AKIRA Headphones</Link>
+      </p>
+      <button type="button" className="cart-icon">
+        <AiOutlineShoppingCart />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;

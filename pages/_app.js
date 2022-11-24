9a3b47ffd4ef903fdb36369cc67fffb0baf84de0
Junrong Fu <junrongfu6@gmail.com>
2022-11-24 23:44:38 +0800

    add someting

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
-import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,7 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ products, product }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const [decQty, incQty, qty] = useStateContext();
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -69,7 +69,11 @@ const ProductDetails = ({ products, product }) => {
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">

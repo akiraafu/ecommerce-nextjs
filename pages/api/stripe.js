16e18ae22869114549ec61ca970b01e1a8d8c8cb
Junrong Fu <junrongfu6@gmail.com>
2022-11-26 20:32:40 +0800

    add stripe

---
+++ pages/api/stripe.js
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "post") {
+    try {
+    } catch (error) {}
+  }
+}
